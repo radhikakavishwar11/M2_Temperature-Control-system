06ccf05424b168e7d67a1aaed98bca675bbc261b
Radhika Kavishwar <kavishwarrt@rknec.edu>
2022-04-21 14:26:37 +0530

    done

---
+++ 3_Implementation/src/Temp.c
@@ -0,0 +1,25 @@
+void Init_ADC(){
+    SET_REFERENCE_VOLTAGE;
+    ADC_ENABLE_PRESCALE;
+
+}
+
+/**
+ * @brief A funciton to receive temperature sensor inputs from analog pin and output it to a register ADC which has 2 8 bit registers ADCL and ADCH.
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+int_t Read_ADC(int_t ch)
+{
+    ADMUX&=0xf8;
+    ch = ch&0b00000111;
+    ADMUX|=ch;
+
+    ADCSRA|=(1<<ADSC);
+
+    while(ADC_INTERRUPT_NOT_OCCURED);
+    SET_ADC_INTERRUPT_FLAG;
+    return(ADC);
+
+}
